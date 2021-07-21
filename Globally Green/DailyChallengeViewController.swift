2e339d342521c19ca129f9c2c3b3d380ceb8072c
Medha <medha.rajan04@gmail.com>
2021-07-21 15:02:07 -0500

    i added the challenges. I need to add more challenges and fix the font tomorrow.

--- Globally Green/DailyChallengeViewController.swift
+++ Globally Green/DailyChallengeViewController.swift
@@ -8,11 +8,22 @@
 import UIKit
 
 class DailyChallengeViewController: UIViewController {
-
+    let easy = ["Open your windows at least 5 minutes each day and reduce the indoor air pollution in your home","Take short showers instead of baths","Purify the air in your home with indoor plants. Plants clean the air from toxins and produce oxygen"]
+    let hard = ["Use soap bars instead of liquid soaps","Use safe and environmentally-friendly cleaning products (ex. baking soda, soap flakes, lemon, cornstarch and vinegar)"]
+    @IBOutlet weak var challenge: UILabel!
     @IBOutlet var pagesCollection: [UIButton]!
     
+    @IBAction func hardBtn(_ sender: Any) {
+        challenge.text = easy.randomElement()
+        challenge.font = UIFont(name:"Helvetica", size: 20.0)
+    }
+    @IBAction func easyBtn(_ sender: Any) {
+        challenge.text = easy.randomElement()
+        challenge.font = UIFont(name:"Helvetica", size: 20.0)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        challenge.numberOfLines = 0
 
         // Do any additional setup after loading the view.
     }
--- Globally Green/ViewController.swift
+++ Globally Green/ViewController.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var quote: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
