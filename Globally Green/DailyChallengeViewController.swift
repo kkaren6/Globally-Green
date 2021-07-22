6836ddfc9056ac4d593c4f2e7453de87d6354b2e
Medha <medha.rajan04@gmail.com>
2021-07-22 10:36:19 -0500

    I fixed the generate button and added some more code

--- Globally Green/DailyChallengeViewController.swift
+++ Globally Green/DailyChallengeViewController.swift
@@ -8,18 +8,18 @@
 import UIKit
 
 class DailyChallengeViewController: UIViewController {
-    let easy = ["Open your windows at least 5 minutes each day and reduce the indoor air pollution in your home","Take short showers instead of baths","Purify the air in your home with indoor plants. Plants clean the air from toxins and produce oxygen"]
-    let hard = ["Use soap bars instead of liquid soaps","Use safe and environmentally-friendly cleaning products (ex. baking soda, soap flakes, lemon, cornstarch and vinegar)"]
+    let easy = ["Open your windows at least 5 minutes each day and reduce the indoor air pollution in your home","Take short showers instead of baths","Purify the air in your home with indoor plants. Plants clean the air from toxins and produce oxygen", "Use old clothes as cleaning rags","Avoid purchasing bottled water","Choose to have receipts emailed", "Pasta/rice water is great for watering plants: full of starch and rich in minerals and vitamins, it acts as a natural fertilizer (make sure it is not salted though!)", "Bring in natural light more often (instead of making your electricity bill go higher)","Hang or line-dry your laundry instead of using a tumble dryer", "Use soap bars instead of liquid soaps"]
+    let hard = ["Use safe and environmentally-friendly cleaning products (ex. baking soda, soap flakes, lemon, cornstarch and vinegar)", "Make your own hand sanitizer using rubbing alcohol, aloe vera gel, essential oil drops, and a reusable bottle", "Buy furniture made from reclaimed wood or recycled materials", "Buying loose tea and using a tea strainer requires zero-waste and are generally of higher quality because they are made of whole, unbroken leaves compared to individual tea bags"]
     @IBOutlet weak var challenge: UILabel!
     @IBOutlet var pagesCollection: [UIButton]!
     
     @IBAction func hardBtn(_ sender: Any) {
-        challenge.text = easy.randomElement()
-        challenge.font = UIFont(name:"Helvetica", size: 20.0)
+        challenge.text = hard.randomElement()
+        challenge.font = UIFont(name:"Helvetica Light", size: 20.0)
     }
     @IBAction func easyBtn(_ sender: Any) {
         challenge.text = easy.randomElement()
-        challenge.font = UIFont(name:"Helvetica", size: 20.0)
+        challenge.font = UIFont(name:"Helvetica Light", size: 20.0)
     }
     override func viewDidLoad() {
         super.viewDidLoad()
