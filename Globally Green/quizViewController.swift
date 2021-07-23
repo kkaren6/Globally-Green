a78ebcfd04a49ac6aa307dfcff52898766e06367
Katherine Li <katherine.li.tx@gmail.com>
2021-07-23 07:35:12 -0500

    changed font on quiz

--- Globally Green/quizViewController.swift
+++ Globally Green/quizViewController.swift
@@ -13,7 +13,8 @@ class quizViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        numberCorrect = 0
         // Do any additional setup after loading the view.
     }
     
